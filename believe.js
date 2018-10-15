eb08c5edbc1260531fb10dc91683d815709ab905
Pmc Dev <phoomparin@gmail.com>
2018-10-15 00:33:44 -0700

    :lipstick: UI Design Changes

--- believe.js
+++ believe.js
@@ -1,122 +1,126 @@
-const template = props => `
-  <style>
-    .scene {
-      position: fixed;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
+(() => {
+  // prettier-ignore
+  const quotes = [
+    'I believe in you!',
+    'Take a deep breath.',
+    'Just Breathe.',
+    'You can do it!',
+    "You're almost there!",
+    'Believe in yourself.',
+    'Breathe in.'
+  ]
 
-      // background: #2d2d30;
-      background: linear-gradient(45deg, #93278f, #00a99d);
-      // background: linear-gradient(45deg, #3a3897, #a3a1ff);
+  function template({quote, error}) {
+    const stackOverflow = encodeURI(`https://stackoverflow.com/search?q=[js] ${error.message}`)
 
-      transition: opacity 0.4s ease-in-out;
-    }
+    return `
+      <div class="scene error-scene" style="opacity: 0">
+      <link rel='stylesheet' href='./believe.css' />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400">
 
-    #particles {
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-    }
+        <div id="particles"></div>
 
-    .container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      min-height: 100vh;
+        <div class="close-button">
+          <svg viewBox="0 0 24 24">
+            <path d="M18.984 6.422l-5.578 5.578 5.578 5.578-1.406 1.406-5.578-5.578-5.578 5.578-1.406-1.406 5.578-5.578-5.578-5.578 1.406-1.406 5.578 5.578 5.578-5.578z"></path>
+          </svg>
+        </div>
 
-      z-index: 2;
-    }
+        <div class="backdrop">
+          <div class="container">
+            <h1 style="opacity: 0">${quote}</h1>
 
-    .error {
-      transition: opacity 0.4s ease-in-out;
-    }
-
-    .container h1, .container h2 {
-      color: #fefefe;
-      font-weight: 300;
-      font-family: "Helvetica Neue";
-
-      transition: opacity 0.4s ease-in-out;
-    }
-
-    .container h1 {
-      font-size: 5em;
-    }
-
-    .container h2 {
-      font-size: 1.7em;
-    }
-  </style>
-
-  <div class="scene error-scene" style="opacity: 0">
-    <div id='particles'></div>
-
-    <div class="container">
-      <h1 style="opacity: 0">${props.quote}</h1>
-
-      <div class="error" style="opacity: 0">
-        <h2>${props.error}</h2>
+            <div class="error" style="opacity: 0">
+              <h2>
+                <a href="${stackOverflow}" target="_blank">
+                  ${error.name}: ${error.message}
+                </a>
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-`
+    `
+  }
 
-// prettier-ignore
-const quotes = [
-  'I believe in you!',
-  'Take a deep breath.',
-  'Just Breathe.',
-  'You can do it!',
-  "You're almost there!",
-  'Believe in yourself.',
-  'Breathe in.'
-]
+  const random = list => list[Math.floor(Math.random() * list.length)]
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const random = list => list[Math.floor(Math.random() * list.length)]
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+  const db = {
+    errors: 0,
+    key: 'stats.errors',
 
-async function onError({error}) {
-  const quote = random(quotes)
-
-  const html = template({
-    quote: '',
-    error: error.message
-  })
-
-  document.write(html)
-
-  const scene = document.querySelector('.scene')
-
-  await delay(60)
-  scene.style.opacity = 1
-
-  particlesJS.load('particles', 'particles.json')
-
-  const heading = scene.querySelector('h1')
-  
-  for (const char of quote.split('')) {
-    await delay(60)
+    save(value) {
+      localStorage.setItem(this.key, JSON.stringify(this))
+    },
     
-    heading.textContent += char
+    load() {
+      const stats = JSON.parse(localStorage.getItem(this.key))
 
-    if (heading.textContent.length === 1) {
-      heading.style.opacity = 1
+      for (const stat in stats) {
+        this[stat] = stats[stat]
+      }
+
+      return stats
     }
   }
 
-  await delay(200)
+  async function onError({error}) {
+    db.load()
+    db.errors += 1
+    db.save()
 
-  const subheading = scene.querySelector('.error')
-  subheading.style.opacity = 1
+    const quote = random(quotes)
 
-  return true
-}
+    const html = template({
+      quote: '',
+      error
+    })
 
-window.addEventListener("error", onError)
+    const rootNode = document.createElement('div')
+    rootNode.innerHTML = html
+    document.body.appendChild(rootNode)
 
-console.log('[+] Error Handling Active')
+    const scene = rootNode.querySelector('.scene')
+
+    await delay(60)
+    scene.style.opacity = 1
+
+    particlesJS.load('particles', 'particles.json')
+
+    const closeButton = scene.querySelector('.close-button')
+
+    closeButton.addEventListener('click', async () => {
+      await delay(60)
+
+      scene.style.opacity = 0
+
+      await delay(1800)
+
+      document.body.removeChild(rootNode)
+    })
+
+    const heading = scene.querySelector('h1')
+    
+    for (const char of quote.split('')) {
+      await delay(60)
+      
+      heading.textContent += char
+
+      if (heading.textContent.length === 1) {
+        heading.style.opacity = 1
+      }
+    }
+
+    await delay(200)
+
+    const subheading = scene.querySelector('.error')
+    subheading.style.opacity = 1
+
+    return true
+  }
+
+  window.addEventListener("error", onError)
+
+  console.log('[+] Error Handling Active')
+})()
